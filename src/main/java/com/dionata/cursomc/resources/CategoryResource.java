c6c874cbd61d51c92389ecafa9f2ff4b425bb5b7
araujoDionata <araujo.dionata@gmail.com>
2018-09-18 10:14:40 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/dionata/cursomc/resources/CategoryResource.java
@@ -0,0 +1,29 @@
+package com.dionata.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dionata.cursomc.domain.Category;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoryResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Category> listar() {
+
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+
+		List<Category> list = new ArrayList<>();
+
+		list.add(cat1);
+		list.add(cat2);
+		return list;
+	}
+
+}
