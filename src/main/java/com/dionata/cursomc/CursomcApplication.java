5aaf3afdf08f9e83c30d393b3564ea47168614d1
araujoDionata <araujo.dionata@gmail.com>
2018-09-18 11:28:42 -0300

    Criando operacao de instanciacao

--- src/main/java/com/dionata/cursomc/CursomcApplication.java
+++ src/main/java/com/dionata/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.dionata.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dionata.cursomc.domain.Category;
+import com.dionata.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 }
