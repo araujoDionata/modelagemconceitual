498b1cb46799d77d437f92d514a0aeca51badef0
araujoDionata <araujo.dionata@gmail.com>
2018-09-21 15:13:52 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/dionata/cursomc/resources/CategoryResource.java
+++ src/main/java/com/dionata/cursomc/resources/CategoryResource.java
@@ -34,11 +34,18 @@ public class CategoryResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Category obj, @PathVariable Integer id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Category obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/dionata/cursomc/services/CategoryService.java
+++ src/main/java/com/dionata/cursomc/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.dionata.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dionata.cursomc.domain.Category;
 import com.dionata.cursomc.repositories.CategoryRepository;
+import com.dionata.cursomc.services.exceptions.DataIntegrityException;
 import com.dionata.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,4 +33,14 @@ public class CategoryService {
 		return repo.save(obj);
 	}
 
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+
+	}
 }
