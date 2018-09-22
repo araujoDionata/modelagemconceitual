8a6d894c95f96b5e4b58088d2c6bbaf91ce18b07
araujoDionata <araujo.dionata@gmail.com>
2018-09-22 20:41:16 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/dionata/cursomc/services/CategoryService.java
+++ src/main/java/com/dionata/cursomc/services/CategoryService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dionata.cursomc.domain.Category;
@@ -47,5 +50,10 @@ public class CategoryService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 
+	}
+
+	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
